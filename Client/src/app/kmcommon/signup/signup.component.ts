dc268c1eb2db3dd37d87b4a50544ab4b1ea4d206
imaheshnagar <imaheshnagar@gmail.com>
2022-11-04 20:26:25 +0530

    adding validation in signup
    adding validation in signup

--- Client/src/app/kmcommon/signup/signup.component.ts
+++ Client/src/app/kmcommon/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { EmailValidator, FormControl, FormGroup, Validators } from '@angular/forms';
+import { EmailValidator, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mg-signup',
@@ -13,28 +13,59 @@ export class SignupComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+
   fg = new FormGroup(
     { 
-      fname:new FormControl('',[Validators.required]),
+      fname:new FormControl('',[Validators.required,this.testNotAllowed]),
       lname:new FormControl('',[Validators.required]),
       address:new FormGroup(
         {
            line1:new FormControl('',[Validators.required]),
            line2:new FormControl('',[Validators.required]),
+           country:new FormControl('',[Validators.required]),
+           state:new FormControl('',[Validators.required]),
            city:new FormControl('',[Validators.required]),
         }
       ),
-      mobile:new FormControl('',[Validators.required]),
+      mobile:new FormControl('',[Validators.required,Validators.pattern('^ [0 - 9]{10}$')]),
       email:new FormControl('',[Validators.required,Validators.email]),
       psw:new FormControl('',[Validators.required]),
       cnfpsw:new FormControl('',[Validators.required]),
       acceptTerms:new FormControl(false)
-    }
+    },
+    [this.PasswordMatch]
   )
 
   signup()
   {
     console.log(this.fg);
+  }
+
+  testNotAllowed():ValidatorFn
+  {
+    return ():ValidationErrors|null=>
+    {
+      const name = this.fg.get('fname');
+      return name?.value =="test" ? null :{notAllowed:{value:true}};
+    }
+  }
+
+  PasswordMatch():ValidatorFn
+  {
+      return():ValidationErrors|null=>
+      {
+        const psw = this.fg.get('psw');
+        const cnfpsw= this.fg.get('cnfpsw');
+        if(psw?.value && cnfpsw?.value && psw.value == cnfpsw.value)
+        {
+          return null ;
+        }
+        else
+        {
+          return {passwordNotMatch:{value:true}};
+        }
+      };
   }
 
   kmErrorMessage(cntrl:string,fg:FormGroup):string
