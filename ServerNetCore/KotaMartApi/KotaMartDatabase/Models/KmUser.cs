1bba30f07a8a41a24e9326853789a042d4ff6136
imaheshnagar <imaheshnagar@gmail.com>
2022-11-08 20:27:04 +0530

    updated code

---
+++ ServerNetCore/KotaMartApi/KotaMartDatabase/Models/KmUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KotaMartDatabase.Models
+{
+    public class KmUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public virtual ICollection<KmRole> Roles { get; set; }
+   
+    }
+}
